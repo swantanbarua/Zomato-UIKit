abba6116cffdf2112358e5e0418f65c3894ef3b9
Swantan Barua <work.swantan@gmail.com>
2024-11-04 14:13:02 +0530

    Initial Commit

---
+++ Zomato-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zomato-UIKit
+//
+//  Created by Swantan Barua on 04/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Zomato-UIKitTests/Zomato_UIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Zomato_UIKitTests.swift
+//  Zomato-UIKitTests
+//
+//  Created by Swantan Barua on 04/11/24.
+//
+
+import Testing
+@testable import Zomato_UIKit
+
+struct Zomato_UIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
